3d7f7f2b0657da5a34cbf3ef8ad9960b1a922466
andersf-sec <71634379+andersf-sec@users.noreply.github.com>
2023-08-25 10:20:45 +0200

    shares and services

---
+++ sanitycheck.ps1
@@ -0,0 +1,6 @@
+﻿$thehost=hostname
+$shares=get-smbshare|foreach-object {Get-SmbShareAccess $_.Name}
+$net=netstat -anb
+$thehost,$shares,$net|out-string
+netsh advfirewall show currentprofile
+gwmi  win32_service |foreach-object {if ($_.pathname -match '.* .*\\'){if ($_.pathname -match '"'){"----------------",$_.name,$_.pathname,$_.state,$_.StartName,$_.StartMode}}}
